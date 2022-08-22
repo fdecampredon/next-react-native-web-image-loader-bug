49ffade94679aee2fa57274594bd29969afe66a2
François de Campredon <francois.de.campredon@gmail.com>
2022-08-22 13:19:39 +0200

    bug reproduction

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/,
+      options: {
+        name: '/static/media/[name].[hash:8].[ext]',
+        esModule: false,
+        scalings: { '@2x': 2, '@3x': 3 },
+      },
+      loader: 'react-native-web-image-loader',
+    });
+    return config;
+  },
 }
 
 module.exports = config
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 /** Add your relevant code here for the issue to reproduce */
+require('../assets/dog.jpeg');
+
 export default function Home() {
   return null
 }
